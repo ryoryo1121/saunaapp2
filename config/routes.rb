3d436497964c980a955ce5312db0511bbd9194e6
ryo ikeda <ry01121xx@gmail.com>
2019-01-09 21:22:57 +0900

    add logo image

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/contact' => 'home#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
