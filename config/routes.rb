2d02bf5221e6a19caa04f2b9272ce434a5eab6d3
ryo ikeda <ry01121xx@gmail.com>
2019-01-14 22:10:32 +0900

    114

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/contact' => 'home#contact'
 
+  get '/users/login' => 'users#sign_up'
+  resources :users
+
   resources :facilities
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
