f56bec914d1887c4c40f7857fc27fb1908308551
ryo ikeda <ry01121xx@gmail.com>
2019-01-20 11:15:51 +0900

    add facility model

--- db/migrate/20190113013941_create_facilities.rb
+++ db/migrate/20190113013941_create_facilities.rb
@@ -1,6 +1,18 @@
 class CreateFacilities < ActiveRecord::Migration[5.2]
   def change
     create_table :facilities do |t|
+      t.string :name, null: false
+      t.string :description
+      t.string :addressß
+
+      t.integer :sauna_temp
+      t.integer :mizuburo_temp
+
+      t.boolean :sauna_tv
+
+      has_one_attached :image
+      # アクティブストレージを利用する予定
+
 
       t.timestamps
     end
