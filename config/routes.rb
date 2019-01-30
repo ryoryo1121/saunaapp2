a7c286aa4de99be1b2b2e3f3963ecf2b2a229d30
ryo ikeda <ry01121xx@gmail.com>
2019-01-30 16:23:57 +0900

    user crud1

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   devise_for :users
   root 'home#index'
   get '/contact' => 'home#contact'
-
-  get '/users/login' => 'users#sign_up'
-  resources :users
-
+  resources :users, :only => [:index, :show]
   resources :facilities
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
