caf9dcead6140dded6f299bff58f6957db8c3bb3
ryo ikeda <ry01121xx@gmail.com>
2019-03-29 14:19:06 +0900

    no message

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   def index
       @facilities = Facility.page(params[:page]).per(5)
+      @comment = Comment.page(params[:page]).per(5)
   end
   def contact
   end
